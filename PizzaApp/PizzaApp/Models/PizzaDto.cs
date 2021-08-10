29e7c13b4b19287feffff97540c70cc2110042b8
Anoop Sasidharan <anoopas.as@gmail.com>
2021-08-10 13:19:06 -0400

    adding add validation

--- PizzaApp/PizzaApp/Models/PizzaDto.cs
+++ PizzaApp/PizzaApp/Models/PizzaDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace PizzaApp.Models
 {
     public class PizzaDto
     {
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
+        [Required]
+        [Range(1,100)]
         public double Price { get; set; }
     }
 }
